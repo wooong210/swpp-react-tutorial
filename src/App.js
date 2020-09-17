4fab7361fd1010ecd6de5558f1a7e67833b849a2
wooong210 <w980417@gmail.com>
2020-09-17 20:17:42 +0900

    make TodoList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import './App.css';
+import NewTodo from './containers/TodoList/NewTodo/NewTodo';
+import TodoList from './containers/TodoList/TodoList'; // can omit .js ...
+import TodoDetail from './components/TodoDetail/TodoDetail';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Switch>
+          <Route path='/todos' exact render={() => <TodoList title="My TODOs!" />} />
+          <Route path='/new-todo' exact component={NewTodo} />
+          <Redirect exact from='/' to='/todos' />
+          <Route render={() => <h1>Not Found</h1>} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
